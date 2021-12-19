8f81d9e61dab306f3cb820b9f3b69421698ffb29
alex <alex@home.fr>
2021-12-19 06:37:05 +0100

    introducing bash completion

---
+++ bash-completion-to-source.sh
@@ -0,0 +1,21 @@
+
+__makefile_maka_complete()
+{
+    local words
+    local cur="${COMP_WORDS[COMP_CWORD]}"
+    if [[ -n "$cur" && t = "${cur:0:1}" ]]; then
+        words=$( for f in src/tests/Test*.cpp; do if [[ -f $f ]]; then ff=$(basename $f); fff=${ff%%.cpp}; echo test-${fff#Test}; fi; done )
+    elif [[ -n "$cur" && gdb-t = "${cur:0:5}" ]]; then
+        words=$( for f in src/tests/Test*.cpp; do if [[ -f $f ]]; then ff=$(basename $f); fff=${ff%%.cpp}; echo gdb-test-${fff#Test}; fi; done )
+    elif [[ ${#COMP_WORDS[@]} -eq 2 ]]; then
+        words="all release debug clean clean-tests check recheck go gdb gdb-debug gdb-release gdb-test test-"
+    #elif [[ "BUILD_MODE=" = "$cur" ]]; then
+    #    words="BUILD_MODE=debug BUILD_MODE=release"        
+    fi
+    if [[ ${#COMP_WORDS[@]} -ge 2 ]]; then
+        words="$words BUILD_MODE=debug BUILD_MODE=release"        
+    fi
+    COMPREPLY=($(compgen -W "$words" -- "$cur" ))
+}
+
+complete -F __makefile_maka_complete make
