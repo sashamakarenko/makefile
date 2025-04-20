d59e375b8f8e6e70b3e540bf3e9c3b0be26eb157
sashamakarenko <sashamakarenko@users.noreply.github.com>
2025-04-20 06:41:13 +0200

    make bash completion fixed

--- bash-completion-to-source.sh
+++ bash-completion-to-source.sh
@@ -10,7 +10,7 @@ __makefile_maka_complete()
     elif [[ -n "$cur" && r = "${cur:0:1}" ]]; then
         words=$( for f in src/tests/Test*.cpp; do if [[ -f $f ]]; then ff=$(basename $f); fff=${ff%%.cpp}; echo retest-${fff#Test}; fi; done )
     elif [[ ${#COMP_WORDS[@]} -eq 2 ]]; then
-        words="$(grep '^\w.*:' Makefile | grep -v = | egrep -v % | grep -v \$ | cut -f1 -d:) all release debug clean clean-tests clean-lcov lcov check recheck go gdb ldd gdb-debug gdb-release gdb-test- build-all-tests rebuild rebuild-all test- new-class generate-vscode show-var-"
+        words="$(grep '^\w.*:' Makefile | grep -v = | egrep -v % | grep -v '\$' | cut -f1 -d:) all release debug clean clean-tests clean-lcov lcov check recheck go gdb ldd gdb-debug gdb-release gdb-test- build-all-tests rebuild rebuild-all test- new-class generate-vscode show-var-"
     fi
     if [[ ${#COMP_WORDS[@]} -ge 2 ]]; then
         if [[ ${COMP_WORDS[1]} = new-class ]]; then
